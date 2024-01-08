eb7f2e9b3e288eef89efe1887055a822cd9703f9
Shepetkov Timerlan <sergeevp284@gmail.com>
2024-01-08 10:29:21 +0300

    home work ready

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+
+create table customers(
+customer_id char(5)primary key,
+company_name varchar(50),
+contact_name VARCHAR(50)
+);
+
+create table employees(
+employee_id SERIAL primary key,
+first_name varchar(50),
+last_name varchar(100),
+title varchar(50),
+birth_date date,
+notes TEXT);
+
+
+create table orders(
+order_id serial primary key,
+customer_id varchar(5),
+employee_id serial,
+order_date date,
+ship_city text
+);
+
+
+SELECT * FROM customers;
+SELECT * FROM employees;
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="Shepetok2000"
+)
+
+cur = conn.cursor()
+# Открытие файла с данными
+
+
+with open('north_data/customers_data.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute("INSERT INTO customers(customer_id, company_name, contact_name) VALUES (%s, %s, %s)", row)
+
+with open('north_data/employees_data.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute(
+            "INSERT INTO employees(employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+            row)
+
+with open('north_data/orders_data.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute(
+            "INSERT INTO orders(order_id, customer_id, employee_id, order_date, ship_city ) VALUES (%s, %s, %s, %s, %s)",
+            row)
+
+conn.commit()
+conn.close()
